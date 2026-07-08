cd719ee95d65d9dca3be858bed0ea6cfdac5de3b
Pumacode04 <cransonlim@yahoo.com.sg>
2026-07-08 08:55:09 +0000

    Jul 8, 2026, 8:55 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -16,7 +16,7 @@ function setup() {
   circle(400, 200, 100) //x, y, diameter
   rect(500, 200, 100, 200) // x, y, width, height
   triangle(400, 117.5, 350, 200, 450, 200) // x1, y1, x2, y2, x3, y3
-  quad(150, 100, 250, 200, 150, 175, 50, 200) // x1, y1, x2, y2, x3, y3, x4, y4 // I'm not doing that.
+  quad(150, 100, 225, 200, 150, 175, 75, 200) // x1, y1, x2, y2, x3, y3, x4, y4 // I'm not doing that.
 
    // Create a bouncing ball sprite
    // write your codes here
