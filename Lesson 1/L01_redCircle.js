2994c91d84ca3b97dd6e83ef43a96803b415aee2
Pumacode04 <cransonlim@yahoo.com.sg>
2026-07-08 08:06:39 +0000

    Jul 8, 2026, 8:06 AM

--- Lesson 1/L01_redCircle.js
+++ Lesson 1/L01_redCircle.js
@@ -6,6 +6,10 @@ function setup(){
 function draw(){
     fill(255, 0, 0);
     ellipse(300,200,200,200);
+
+    for (let i = 0; i < 5; i++) {
+        
+    }
 }
 
 // green : 4efc03. Red : fc0000. Yellow : ffcc000. Blue: 00ccff
