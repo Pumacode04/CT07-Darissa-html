6077bfa6994d8598633b89307f4ddbc12f5bdeaf
Pumacode04 <cransonlim@yahoo.com.sg>
2026-07-15 07:50:27 +0000

    Jul 15, 2026, 7:50 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -18,7 +18,7 @@ function setup() {
   console.log(area)
   textSize(100)
   fill("Navy")
-  text(area, 350, 250)
+  text("Ärea : "area, 350, 250)
 
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
