2dae14ff68e58c1ab2b47b7196b796f5829e2037
Pumacode04 <cransonlim@yahoo.com.sg>
2026-07-15 07:57:17 +0000

    Jul 15, 2026, 7:57 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -23,7 +23,7 @@ function setup() {
 
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here
-  for (let i = 2; i < 21; i++){
+  for (let i = 2; i < 21; i += 2){
     console.log(i);
   }
   // --- Exercise: Age category classification ---
