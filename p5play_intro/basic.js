99908d5e9ca794c4249d1aa92beb8a63106d55f2
Pumacode04 <cransonlim@yahoo.com.sg>
2026-07-15 08:31:02 +0000

    Jul 15, 2026, 8:31 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -68,7 +68,7 @@ function setup() {
   groceries.push("Ice cream");
   groceries.shift();
   groceries.pop();
-  groceries.splice(1, 1); // 
+  groceries.splice(1, 1); // (num of items to remove, )
 
   console.log(groceries);
 }
