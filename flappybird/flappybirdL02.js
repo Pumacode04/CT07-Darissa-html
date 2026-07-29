2c2a1d4d80c44a3d7c8214e58ccf5383af99573c
Pumacode04 <cransonlim@yahoo.com.sg>
2026-07-29 07:58:16 +0000

    Jul 29, 2026, 7:58 AM

--- flappybird/flappybirdL02.js
+++ flappybird/flappybirdL02.js
@@ -75,6 +75,8 @@ function draw() {
     text("isMoving:" + bird.isMoving, 10, 40);
     text("sleeping:"+ bird.sleeping, 10, 60);
 
-    if (frameCount === 1) {}
+    if (frameCount === 1) {
+        spawn
+    }
 
 }
