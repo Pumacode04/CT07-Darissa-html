f5718f209151a945a296c8fcb252636a03b41269
Pumacode04 <cransonlim@yahoo.com.sg>
2026-07-29 07:43:16 +0000

    Jul 29, 2026, 7:43 AM

--- flappybird/flappybirdL02.js
+++ flappybird/flappybirdL02.js
@@ -9,7 +9,8 @@ let base;
 
 function preload() {
     birdMidImg = loadImage("assets/yellowbird-midflap.png");
-    birdUpImg = loadImage("assets/yellowbird-midflap.png");
+    birdUpImg = loadImage("assets/yellowbird-upflap.png");
+    
     background = loadImage("assets/background-day.png");
     base = loadImage("assets/base.png");
 }
