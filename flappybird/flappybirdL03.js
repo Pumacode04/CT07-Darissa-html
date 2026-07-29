652139b249f87bb5e8e9112327d4558b1906ba1f
Pumacode04 <cransonlim@yahoo.com.sg>
2026-07-29 08:18:34 +0000

    Jul 29, 2026, 8:18 AM

--- flappybird/flappybirdL03.js
+++ flappybird/flappybirdL03.js
@@ -88,7 +88,7 @@ function draw() {
     floor.x = bird.x; // "locl"the floor pos to the bird.x pos
 
     if (frame % 90 === 0){
-        spawnPipePair(); 
+        spawnPipePair(); // spawn pipe every 1.5 seconds
     }
 }
 
