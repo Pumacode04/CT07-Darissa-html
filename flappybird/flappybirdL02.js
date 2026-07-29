db04c9a313ef22b424bb20d51f40902100e67c0b
Pumacode04 <cransonlim@yahoo.com.sg>
2026-07-29 07:47:10 +0000

    Jul 29, 2026, 7:47 AM

--- flappybird/flappybirdL02.js
+++ flappybird/flappybirdL02.js
@@ -56,7 +56,7 @@ function draw() {
         bird.sleeping = false; // Make sure no sprite is idle
     }
 
-    if (kb.presses("space" || mouse.presses("left"))) {
+    if (kb.presses()) {
         bird.vel.y = -5; // Up velocity
         bird.sleeping = false; // Make sure no sprite is idle
     }
