383f4334c8e79b61aa21762738ba63ccc46f53c1
Pumacode04 <cransonlim@yahoo.com.sg>
2026-08-05 08:33:18 +0000

    Aug 5, 2026, 8:33 AM

--- flappybird/flappybirdL04.js
+++ flappybird/flappybirdL04.js
@@ -191,7 +191,7 @@ function moveGroup(group, targetX, spacing) {
 
     // Find Left-most X position
     // Shifts the stariing point left, so the entire group has been centered
-    let startX 
+    let startX = (targetX - totasl)
 }
 
 function spawnPipePair() {
