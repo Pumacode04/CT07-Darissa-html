54216fa23b891f813c4dcb43458df57359393719
Pumacode04 <cransonlim@yahoo.com.sg>
2026-08-05 07:54:50 +0000

    Aug 5, 2026, 7:54 AM

--- flappybird/flappybirdL04.js
+++ flappybird/flappybirdL04.js
@@ -28,7 +28,9 @@ function preload() {
     pipe = loadImage("assets/pipe-green.png");
     gameoverImg = loadImage("assets/gameover.png")
     startScreenImg = loadImage("assets/message.png");
-    for (let i = 0; i < 10; i++) {}
+    for (let i = 0; i < 10; i++){
+        
+    }
 }
 
 function setup() {
