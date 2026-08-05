a2fb44a94f16fa0771d41b53d376a395bb6cb26b
Pumacode04 <cransonlim@yahoo.com.sg>
2026-08-05 08:08:44 +0000

    Aug 5, 2026, 8:08 AM

--- flappybird/flappybirdL04.js
+++ flappybird/flappybirdL04.js
@@ -158,7 +158,8 @@ function drawScore(x, y, score, digitWidth, digitHeight) {
     // make it a string sp we can get each digits indivisually rather than a value
     let scoreStr = str(score);
     // let total width taken up by all digits
-    let totalWidth = scoreStr.length * 
+    let totalWidth = scoreStr.length * digitWidth;
+    
 }
 
 function spawnPipePair() {
