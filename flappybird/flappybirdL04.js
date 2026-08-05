44e10de8cc6b405751a6e3a4cbe5deec0af42d72
Pumacode04 <cransonlim@yahoo.com.sg>
2026-08-05 08:05:51 +0000

    Aug 5, 2026, 8:05 AM

--- flappybird/flappybirdL04.js
+++ flappybird/flappybirdL04.js
@@ -156,7 +156,7 @@ function drawScore(x, y, score, digitWidth, digitHeight) {
     // Clear old digit sprites
     scoreDigits.removeAll();
     // make it a string sp we can get each digits indivisually rather than a value
-    
+    let scoreStr
 }
 
 function spawnPipePair() {
