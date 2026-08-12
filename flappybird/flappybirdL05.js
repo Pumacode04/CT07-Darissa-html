f349150ef3004fd7d82e270a59b3439159e7fd3b
Pumacode04 <cransonlim@yahoo.com.sg>
2026-08-12 07:56:17 +0000

    Aug 12, 2026, 7:56 AM

--- flappybird/flappybirdL05.js
+++ flappybird/flappybirdL05.js
@@ -40,6 +40,7 @@ function preload() {
         numberImages[i] = loadImage('assets/'+ i + '.png');
 
     flapSound = createAudio("assets/sfx_wing.mp3")
+    pointSound = createAudio("sfx")
     }
 }
 
