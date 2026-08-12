d4c821304b9cc91b0f292ffbf778aedf4e144ed3
Pumacode04 <cransonlim@yahoo.com.sg>
2026-08-12 07:51:10 +0000

    Aug 12, 2026, 7:51 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -23,7 +23,7 @@ let startScreenImg;
 let startGame = false; // flag is false at the start
 
 // Sound assets
-let flapSound. pointSound
+let flapSound, pointSound, 
 
 function preload() {
     birdMidImg = loadImage("assets/yellowbird-midflap.png");
