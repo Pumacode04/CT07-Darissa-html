ec91a5d486b3b5e1ca09ac0c7d9bea986a374aa2
Pumacode04 <cransonlim@yahoo.com.sg>
2026-08-19 08:05:15 +0000

    Aug 19, 2026, 8:05 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -23,8 +23,8 @@ function setup() {
     peach = new Sprite;
     peach.img = peachWhole;
     peach.width = 50; // How heavy, heavier = affected by gravity more
-    peach.height = 50;
-    peach.mass = 2;
+    peach.height = 50; // Air resistance, higher = more resistance
+    peach.mass = 2; 
     peach.drag = 0.02;
     peach.collider = "dynamic";
 }
