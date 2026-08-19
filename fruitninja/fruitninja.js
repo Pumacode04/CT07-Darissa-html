5207edb486d693569790709c3134860f8101b5f1
Pumacode04 <cransonlim@yahoo.com.sg>
2026-08-19 07:50:18 +0000

    Aug 19, 2026, 7:50 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -13,5 +13,5 @@ function setup() {
 }
 
 function draw() {
-    image(background, 0, 0, width, height); // image(x, y, )
+    image(background, 0, 0, width, height); // image(image, x, y, width, height)
 }
