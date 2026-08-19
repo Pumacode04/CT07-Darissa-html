8bbfba1032dc9ba5650f4eb0079fb11045207901
Pumacode04 <cransonlim@yahoo.com.sg>
2026-08-19 07:57:16 +0000

    Aug 19, 2026, 7:57 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -8,7 +8,7 @@ function preload() {
     // Load image
     background = loadImage("assets/dojobackground.png");
     watermelon = loadImage("assets/watermelonwhole.png")
-    peach = loadImage()
+    peach = loadImage("asset")
 }
 
 function setup() {
