b3a03956432706cca9be9047bc58d5085d81a58f
Pumacode04 <cransonlim@yahoo.com.sg>
2026-08-19 08:13:05 +0000

    Aug 19, 2026, 8:13 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -24,7 +24,7 @@ function setup() {
     peach.img = peachWhole;
     peach.width = 50;
     peach.height = 50;
-    peach.s
+    peach.scale = 1
     peach.mass = 2; // How heavy, heavier = affected by gravity more
     peach.drag = 0.02; // Air resistance, higher = more resistance
     peach.collider = "dynamic"; // Collidable, movable and affected by physics
