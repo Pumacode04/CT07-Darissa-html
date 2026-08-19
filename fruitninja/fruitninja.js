222c161a4e2527d18fdca60d88ca895c62350c14
Pumacode04 <cransonlim@yahoo.com.sg>
2026-08-19 08:58:52 +0000

    Aug 19, 2026, 8:58 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -49,8 +49,8 @@ function spawnPeach() {
     // Create peach sprite
     peach =  new peachGroup.Sprite();
     peach.img = peachWhole;
-    peach.width = 50;
-    peach.height = 50;
+    peach.width = 100;
+    peach.height = 100;
     peach.scale = peach.width / peachWhole.width;
     peach.mass = 2; // How heavy, heavier = affected by gravity more
     peach.drag = 0.02; // Air resistance, higher = more resistance
