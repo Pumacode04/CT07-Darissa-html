a4fa4f0672a4114601b3c1ffaefd368941bfb34d
Pumacode04 <cransonlim@yahoo.com.sg>
2026-08-26 07:40:34 +0000

    Aug 26, 2026, 7:40 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -17,18 +17,7 @@ function setup() {
     new Canvas(800, 600);
     world.gravity.y = 10;
 
-    // Sprites
-    watermelon = new Sprite;
-    watermelon.img = watermelonWhole;
-    watermelon.width = 115;
-    watermelon.height = 115;
-    watermelon.scale = watermelon.width / watermelon.width;
-    watermelon.mass = 2; // How heavy, heavier = affected by gravity more
-    watermelon.drag = 0.02; // Air resistance, higher = more resistance
-    watermelon.collider = "dynamic"; // Collidable, movable and affected by physics
-
-    // Group
-    peachGroup = new Group();
+  
 }
 
 function draw() {
@@ -44,14 +33,3 @@ function draw() {
     }
 }
 
-function spawnPeach() {
-    // Create peach sprite
-    peach =  new peachGroup.Sprite();
-    peach.img = peachWhole;
-    peach.width = 100;
-    peach.height = 100;
-    peach.scale = peach.width / peachWhole.width;
-    peach.mass = 2; // How heavy, heavier = affected by gravity more
-    peach.drag = 0.02; // Air resistance, higher = more resistance
-    peach.collider = "dynamic"; // Collidable, movable and affected by physics
-}
