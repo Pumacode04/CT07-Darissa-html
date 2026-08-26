8093aea3febc8468cf95fcbccb2126ca08b38370
Pumacode04 <cransonlim@yahoo.com.sg>
2026-08-26 08:21:57 +0000

    Aug 26, 2026, 8:21 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -10,7 +10,8 @@ function preload() {
     // decalre the peach object
     let peach = {
         whole: loadImage("assets/peachwhole.png"),
-        half1: loadImage("assets/peachhalf.png")
+        half1: loadImage("assets/peachhalf.png"),
+        half2: loadImage()
     }
 
     // declare the watermelon object
