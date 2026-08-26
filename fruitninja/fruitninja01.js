c048d13ff8ffe2ecb453dfa3f834ce8eff2847d5
Pumacode04 <cransonlim@yahoo.com.sg>
2026-08-26 09:00:33 +0000

    Aug 26, 2026, 9:00 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -21,7 +21,7 @@ function preload() {
         half1: loadImage("assets/watermelonhalf.png"),
         half2: loadImage("assets/watermelonhalf.png"),
     }
-   
+    
     // store the fruit objects into an array
     fruitTypes = [peach, watermelon];
 
