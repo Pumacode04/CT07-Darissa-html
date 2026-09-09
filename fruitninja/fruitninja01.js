3f9f9b34fbc43f6e961dfb3b793355cd00e1537b
Pumacode04 <cransonlim@yahoo.com.sg>
2026-09-09 08:52:26 +0000

    Sep 9, 2026, 8:52 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -38,7 +38,7 @@ function preload() {
 
     // declare the kiwi object
     let strawberry = {
-        whole: loadImage("assets/orange1.png"),
+        whole: loadImage("assets/strawberry1.png"),
         half1: loadImage("assets/orange2.png"),
         half2: loadImage("assets/orange2.png"),
     }
