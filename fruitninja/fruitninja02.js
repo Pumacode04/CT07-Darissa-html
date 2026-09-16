e6e53a62ac7749628d5b59b9bec1432724521407
Pumacode04 <cransonlim@yahoo.com.sg>
2026-09-16 09:02:41 +0000

    Sep 16, 2026, 9:02 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -116,6 +116,7 @@ function draw() {
             trail.collider = 'none';
             trail.color = 'red';
             trail.life = 10;
+            fill("red")
             sliceFruit(); // add this line to call function
         }
 
