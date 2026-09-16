5137d395889df444e5c9d3e4ba7bee17d5863fa1
Pumacode04 <cransonlim@yahoo.com.sg>
2026-09-16 07:45:26 +0000

    Sep 16, 2026, 7:45 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -127,7 +127,8 @@ function spawnFruit(){
         let fruit = new fruitGroup.Sprite(randomX, height+20, 40); // spawn at bottom
         fruit.image = fruitData.whole; // load image for whole
 
-        
+        // Size fix - scaleMod
+        fruit.scale = scaleMod;
 
         fruit.type = fruitData; // store reference to its type i.e. peach or watermelon
         fruit.vel.y = random(-10, -14); // shoot upward at random velocity. Adjust to try!
