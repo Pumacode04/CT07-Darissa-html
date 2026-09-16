27eb9f4abb057c1258ae629dc3efb0a1e15c293c
Pumacode04 <cransonlim@yahoo.com.sg>
2026-09-16 08:59:42 +0000

    Sep 16, 2026, 8:59 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -83,7 +83,7 @@ function draw() {
         // Start Menu
         fill("#00124d"); // Text colour
         stroke("");
-        
+        strokeWeight(20);
         textSize(50);
         textAlign(CENTER, CENTER); // (horizontal, vertical) - LEFT, RIGHT, TOP, BOTTOM, CENTER
         text("Fruit Ninja", width / 2, height / 2); // (string, xpos, ypos)
