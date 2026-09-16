259cd77012f9dcdca4cd1408f1865182b7cb537d
Pumacode04 <cransonlim@yahoo.com.sg>
2026-09-16 08:51:05 +0000

    Sep 16, 2026, 8:51 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -81,7 +81,7 @@ function draw() {
     // Draw depending on gameState\
     if (gameState === "start") {
         // Start Menu
-        
+        fill("")
         // Change to playingstate
         if (kb.presses(" ") || mouse.presses()) {
             gameState = "playing";
