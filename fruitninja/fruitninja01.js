10239d2e2022d636fb540cf23810be36e09b971c
Pumacode04 <cransonlim@yahoo.com.sg>
2026-09-16 08:03:38 +0000

    Sep 16, 2026, 8:03 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -83,7 +83,7 @@ function draw() {
 //    text("frameCount:"+ frameCount, 10, 40);
     text("score: " + score, 10, 40);
     fill("fd0000");
-    
+    text("Missed: ")
 
     // call spawnFruit function
     if (frameCount % 120 == 0) {
