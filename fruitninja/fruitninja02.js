5a6037dcc44cc9ef82f934c64fd3a6a197db1e05
Pumacode04 <cransonlim@yahoo.com.sg>
2026-09-16 09:01:02 +0000

    Sep 16, 2026, 9:01 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -87,7 +87,6 @@ function draw() {
         textSize(50);
         textAlign(CENTER, CENTER); // (horizontal, vertical) - LEFT, RIGHT, TOP, BOTTOM, CENTER
         text("Fruit Ninja", width / 2, height / 2); // (string, xpos, ypos)
-        no
         // Change to playing state
         if (kb.presses(" ") || mouse.presses()) {
             gameState = "playing";
