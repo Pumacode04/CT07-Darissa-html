064724a321b408fff6d0caa90cf9e486ae7d3df4
Pumacode04 <cransonlim@yahoo.com.sg>
2026-09-23 08:35:40 +0000

    Sep 23, 2026, 8:35 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -134,7 +134,7 @@ function draw() {
         // floor() || .toFixed()
         fill("#ebe711");
         gameTimer = floor((millis() - gameStartTime) / 1000); // Time since game start
-        
+        // Countdown = duration - time elpased
         text("Time: " + (gameDuration - gameTimer), width / 2, 60);
 
         // Win condition
