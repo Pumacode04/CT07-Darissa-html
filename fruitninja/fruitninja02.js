c8cbe5e534837e5055366934cec90b3379a5b6f4
Pumacode04 <cransonlim@yahoo.com.sg>
2026-09-23 08:31:02 +0000

    Sep 23, 2026, 8:31 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -149,7 +149,7 @@ function draw() {
     } else if (gameState === "win") {
         // Win screen text
         // Game over text / stats
-        fill("#b80000"); // Text colour
+        fill("#b800a9"); // Text colour
         stroke("#000");
         strokeWeight(20);
         textSize(50);
