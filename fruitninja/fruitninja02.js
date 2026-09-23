5e67a9101257a6081ab83fb7f7e3a24c155ff89c
Pumacode04 <cransonlim@yahoo.com.sg>
2026-09-23 08:56:13 +0000

    Sep 23, 2026, 8:56 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -71,7 +71,7 @@ function preload() {
     // store the fruit objects into an array
     fruitTypes = [peach, watermelon, kiwi, orange, strawberry];
 
-    bg
+    bgMusic = loadSound("")
 }
 
 function setup() {
