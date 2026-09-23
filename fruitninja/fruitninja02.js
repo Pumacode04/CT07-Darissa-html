163277b6157a4db1c68a289884ef984cfc944204
Pumacode04 <cransonlim@yahoo.com.sg>
2026-09-23 08:23:34 +0000

    Sep 23, 2026, 8:23 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -132,7 +132,7 @@ function draw() {
 
         // Track time
         gameTimer = (millis() - gameStartTime) / 1000;
-        
+        text("Time: " )
 
         // Win condition
         if (score === 20) {
