ea153154bb91cca51eed01813f75cb4ba27a21a8
Pumacode04 <cransonlim@yahoo.com.sg>
2026-09-23 08:55:15 +0000

    Sep 23, 2026, 8:55 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -6,6 +6,7 @@ let trail;
 let fruitHalves; // new group for sliced halves
 let score = 0; // player's score
 let missed = 0;
+
 let gameState = "start"; // "start", "playing", "gameOver"
 let gameStartTime = 0; // Save time when game starts
 let gameTimer = 0; // Tracks seconds elapsed
