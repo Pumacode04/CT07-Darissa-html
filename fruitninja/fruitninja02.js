798fa94d3cf089271fc126c7befc9963b809563c
Pumacode04 <cransonlim@yahoo.com.sg>
2026-09-23 08:09:38 +0000

    Sep 23, 2026, 8:09 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -7,6 +7,7 @@ let fruitHalves; // new group for sliced halves
 let score = 0; // player's score
 let missed = 0;
 let gameState = "start"; // "start", "playing", "gameOver"
+let 
 
 function preload() {
     // Load image
