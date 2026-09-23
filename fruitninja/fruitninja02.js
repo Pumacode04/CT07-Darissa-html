a6a396a7592a36933af5b9c59b8ad19e1e3f7eb0
Pumacode04 <cransonlim@yahoo.com.sg>
2026-09-23 08:04:10 +0000

    Sep 23, 2026, 8:04 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -143,7 +143,8 @@ function draw() {
         strokeWeight(20);
         textSize(50);
         textAlign(CENTER, CENTER); // (horizontal, vertical) - LEFT, RIGHT, TOP, BOTTOM, CENTER
-        text("Win", width / 2, height * 0.3); // (string, xpos, ypos)
+        text("Win", width / 2, height * 0.5); // (string, xpos, ypos)
+        text("Win", width / 2, height * 0.5); // (string, xpos, ypos)
         return;
     }
     else if (gameState === "lose") {
